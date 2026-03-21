badc0542f9eb58e16e4ebff3a69bfd6e18feee85
Abdul Ahad <ammaar07@gmail.com>
2026-03-21 23:08:58 +0530

    add playRound function

--- script.js
+++ script.js
@@ -1,12 +1,42 @@
 const GAME_OPTIONS = ["ROCK", "PAPER", "SCISSOR"];
+let humanScore = 0;
+let computerScore = 0;
 
 // Function to return either rock, paper or scissor
-const getComputerChoice = () =>
-  console.log(GAME_OPTIONS[Math.floor(Math.random() * 3)]);
+const getComputerChoice = () => GAME_OPTIONS[Math.floor(Math.random() * 3)];
 
 // Function to get human choice
-const getHumanChoice = () => {
-  console.log(prompt("Enter Rock, Paper or Scissor").toUpperCase());
-};
+const getHumanChoice = () =>
+  prompt("Enter Rock, Paper or Scissor").toUpperCase();
 
-getHumanChoice();
+// Function to play a round
+const playRound = (getHumanChoice, getComputerChoice) => {
+  const winCondition =
+    (getComputerChoice === "ROCK" && getHumanChoice === "PAPER") ||
+    (getComputerChoice === "PAPER" && getHumanChoice === "SCISSOR") ||
+    (getComputerChoice === "SCISSOR" && getHumanChoice === "ROCK");
+  const loseCondition =
+    (getComputerChoice === "SCISSOR" && getHumanChoice === "PAPER") ||
+    (getComputerChoice === "ROCK" && getHumanChoice === "SCISSOR") ||
+    (getComputerChoice === "PAPER" && getHumanChoice === "ROCK");
+  const consoleStatement = (result) =>
+    console.log(
+      `Computer: ${getComputerChoice}\nUser: ${getHumanChoice}\nResult: ${result}\n\nUser:${humanScore}\nComputer:${computerScore}`
+    );
+
+  if (getComputerChoice === getHumanChoice) {
+    consoleStatement("Draw");
+  } else if (winCondition) {
+    humanScore += 1;
+    consoleStatement("You Win");
+  } else if (loseCondition) {
+    computerScore += 1;
+    consoleStatement("You Lose");
+  } else {
+    consoleStatement("Wrong Option");
+  }
+};
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
